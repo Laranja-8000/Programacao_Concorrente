b3eb017902ee6994d43ac43899434a08f7297340
Lucas Cavalcante <74940211+Laranja-8000@users.noreply.github.com>
2023-10-13 22:03:19 -0300

    Update atividade3.c

--- Laboratório 4/atividade3.c
+++ Laboratório 4/atividade3.c
@@ -18,28 +18,43 @@
 struct list_node_s* head_p = NULL; 
 //qtde de threads no programa
 int nthreads;
-//lock de exclusao mutua
 
-int cond_leitura = 0, cond_escrita = 0;
+/* variáveis globais */
+pthread_mutex_t x_mutex;
+pthread_cond_t x_cond_read, x_cond_write;
+int leit = 0, escr = 0;
 
 void entraLeitura(){
-   while(cond_leitura != 0){
-      // só espera
+   pthread_mutex_lock(&x_mutex);
+   while(escr > 0){
+      pthread_cond_wait(&x_cond_read, &x_mutex);
    }
+   leit++;
+   pthread_mutex_unlock(&x_mutex);
 }
 
 void saiLeitura(){
-   cond_leitura = 0;
+   pthread_mutex_lock(&x_mutex);
+   leit--;
+   if(leit==0) pthread_cond_signal(&x_cond_write);
+   pthread_mutex_unlock(&x_mutex);
 }
 
 void entraEscrita(){
-   while(cond_escrita != 0){
-      // só espera
+   pthread_mutex_lock(&x_mutex);
+   while((leit>0)||(escr>0)){
+      pthread_cond_wait(&x_cond_write, &x_mutex);
    }
+   escr++;
+   pthread_mutex_unlock(&x_mutex);
 }
 
 void saiEscrita(){
-   cond_escrita = 0;
+   pthread_mutex_lock(&x_mutex);
+   escr--;
+   pthread_cond_signal(&x_cond_write);
+   pthread_cond_broadcast(&x_cond_read);
+   pthread_mutex_unlock(&x_mutex);
 }
 
 
@@ -111,6 +126,11 @@ int main(int argc, char* argv[]) {
       printf("--ERRO: malloc()\n"); return 2;
    }
    
+   // Inicializa o mutex (lock de exclusao mutua) e a variavel de condicao
+   pthread_mutex_init(&x_mutex, NULL);
+   pthread_cond_init (&x_cond_read, NULL);
+   pthread_cond_init (&x_cond_write, NULL);
+
    //cria as threads
    for(long int i=0; i<nthreads; i++) {
       if(pthread_create(tid+i, NULL, tarefa, (void*) i)) {
@@ -136,6 +156,10 @@ int main(int argc, char* argv[]) {
    //libera o espaco de memoria da lista
    Free_list(&head_p);
 
+  /* Desaloca semáforos e termina */
+   pthread_mutex_destroy(&x_mutex);
+   pthread_cond_destroy(&x_cond_read);
+   pthread_cond_destroy(&x_cond_write);
    return 0;
 }  /* main */
 
